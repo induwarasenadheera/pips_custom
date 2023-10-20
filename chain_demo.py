a9b844e26e37111ba220de5150c1e1e80816d973
Induwara Senadheera <755pansilusenadheera@gmail.com>
2023-10-20 14:49:20 +0530

    Add demos

--- chain_demo.py
+++ chain_demo.py
@@ -23,7 +23,7 @@ def run_model(model, rgbs, N, sw):
 
     B, S, C, H, W = rgbs.shape
     rgbs_ = rgbs.reshape(B*S, C, H, W)
-    H_, W_ = 360, 640
+    H_, W_ = 786, 812
     rgbs_ = F.interpolate(rgbs_, (H_, W_), mode='bilinear')
     H, W = H_, W_
     rgbs = rgbs_.reshape(B, S, C, H, W)
@@ -123,7 +123,7 @@ def main():
     S = 50
     N = 1 # number of points to track
 
-    filenames = glob.glob('./demo_images/*.jpg')
+    filenames = glob.glob('./SP250/*.jpg')
     filenames = sorted(filenames)
     print('filenames', filenames)
     max_iters = len(filenames)//(S//2)-1 # run slightly overlapping subseqs
